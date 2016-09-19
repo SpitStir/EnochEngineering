9731e93c6dff6987283792871acc9ed4e8fb7292
Enoch Peterson <Enoch Peterson@SPITSTORM>
2016-09-19 14:05:13 +1000

    Added Logo and under construction

--- eeapp/eeapp/app.js
+++ eeapp/eeapp/app.js
@@ -5,8 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index');
+var about = require('./routes/about');
+var contact = require('./routes/contact');
+var services = require('./routes/services');
+var projects = require('./routes/projects');
+var tools = require('./routes/tools');
+var blog = require('./routes/blog');
 
 var app = express();
 
@@ -23,8 +28,13 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
+app.use('/about', about);
+app.use('/contact', contact);
+app.use('/services', services);
+app.use('/projects', projects);
+app.use('/tools', tools);
+app.use('/blog', blog);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
